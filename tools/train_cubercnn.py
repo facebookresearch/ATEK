0d00c3e36a25093f8202112e1aca3d013b761757
Yang Lou <louy@meta.com>
2024-02-26 23:46:27 -0800

    Enable category-based training.
    Differential Revision: D54237413

--- tools/train_cubercnn.py
+++ tools/train_cubercnn.py
@@ -13,7 +13,7 @@ import torch.distributed as dist
 import yaml
 
 from atek.dataset.atek_webdataset import create_wds_dataloader
-from atek.dataset.omni3d_adapter import create_omni3d_webdataset
+from atek.dataset.omni3d_adapter import create_omni3d_webdataset, ObjectDetectionMode
 from atek.model.cubercnn import build_model
 
 from cubercnn.config import get_cfg_defaults
@@ -78,6 +78,7 @@ def build_test_loader(cfg):
         batch_size=local_batch_size,
         repeat=True,
         category_id_remapping_json=cfg.ID_MAP_JSON,
+        object_detection_mode=ObjectDetectionMode[cfg.DATASETS.OBJECT_DETECTION_MODE],
     )
     test_dataloader = create_wds_dataloader(
         test_wds, num_workers=cfg.DATALOADER.NUM_WORKERS, pin_memory=True
@@ -108,6 +109,7 @@ def build_train_loader(cfg):
         batch_size=local_batch_size,
         repeat=True,
         category_id_remapping_json=cfg.ID_MAP_JSON,
+        object_detection_mode=ObjectDetectionMode[cfg.DATASETS.OBJECT_DETECTION_MODE],
     )
     train_dataloader = create_wds_dataloader(
         train_wds, num_workers=cfg.DATALOADER.NUM_WORKERS, pin_memory=True
