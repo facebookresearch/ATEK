bd029a322b0937fd0a79c3fa5afc5f59863e0238
Yang Lou <louy@meta.com>
2024-06-07 18:07:02 -0700

    Create CubeRCNN Sample builder.
    Summary: This diff creates the SampleBuilder for CubeRCNN. The main difference between this and OBB3 is that it also has OBB2 GT information in the GT dict.
    
    Reviewed By: chengshy
    
    Differential Revision: D58094948
    
    fbshipit-source-id: fc11f62c590d70a07d0c85c9fa87a0edd6aebc6f

---
+++ atek/data_preprocess/sample_builders/cubercnn_sample_builder.py
@@ -0,0 +1,207 @@
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+import logging
+from typing import Dict, Optional
+
+import torch
+
+from atek.data_preprocess.atek_data_sample import (
+    AtekDataSample,
+    MpsTrajData,
+    MultiFrameCameraData,
+)
+from atek.data_preprocess.processors.aria_camera_processor import AriaCameraProcessor
+from atek.data_preprocess.processors.mps_traj_processor import MpsTrajProcessor
+from atek.data_preprocess.processors.obb2_gt_processor import Obb2GtProcessor
+from atek.data_preprocess.processors.obb3_gt_processor import Obb3GtProcessor
+from omegaconf.omegaconf import DictConfig
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class CubeRCNNSampleBuilder:
+    """
+    A sample builder for CubeRCNN model for object detection 3D task
+    """
+
+    def __init__(
+        self,
+        conf: DictConfig,
+        vrs_file: str,
+        mps_files: Dict[str, str],
+        gt_files: Dict[str, str],
+    ) -> None:
+        """
+        vrs_file: the main Aria vrs file.
+        mps_files: A dict that contains:
+            "mps_closedloop_traj_file": closed loop trajectory file.
+        gt_files: A dict that contains the following GT files:
+            "obb3_file": object bounding box 3d file.
+            "obb3_traj_file": object bounding box 3d trajectory file. (TODO: maybe rename this?)
+            "obb2_file": object bounding box 2d file.
+            "instance_json_file": object instance json file that contains object instance information.
+        """
+        self.conf = conf
+
+        self.vrs_file = vrs_file
+
+        self.processors = self._add_processors_from_conf(
+            conf, vrs_file, mps_files if mps_files is not None else {}, gt_files
+        )
+
+    def _add_processors_from_conf(
+        self,
+        conf: DictConfig,
+        vrs_file: str,
+        mps_files: Dict[str, str],
+        gt_files: Dict[str, str],
+    ):
+        """
+        This function creates a dict of processors from the config file.
+        """
+        processors = {}
+        # camera processors
+        camera_conf_list = [
+            conf.rgb,
+            conf.slam_left,
+            conf.slam_right,
+        ]
+        # These fields will be used in creating obb2 processor
+        selected_camera_label_to_stream_ids = {}
+        selected_camera_label_to_pixel_transforms = {}
+        selected_camera_label_to_calib = {}
+        for camera_conf in camera_conf_list:
+            if camera_conf.selected:
+                cam_processor = AriaCameraProcessor(vrs_file, camera_conf)
+                processors[camera_conf.sensor_label] = cam_processor
+                selected_camera_label_to_stream_ids[camera_conf.sensor_label] = (
+                    cam_processor.get_stream_id()
+                )
+                selected_camera_label_to_pixel_transforms[camera_conf.sensor_label] = (
+                    cam_processor.get_pixel_transform()
+                )
+                selected_camera_label_to_calib[camera_conf.sensor_label] = (
+                    cam_processor.get_final_camera_calib()
+                )
+
+        if "mps_traj" in conf and conf.mps_traj.selected:
+            processors["mps_traj"] = MpsTrajProcessor(
+                mps_closedloop_traj_file=mps_files["mps_closedloop_traj_file"],
+                conf=conf.mps_traj,
+            )
+
+        if "cubercnn_gt" in conf and conf.cubercnn_gt.selected:
+            # Cube RCNN GT data contains both obb3 and obb2 data, therefore we create 2 processors
+            # Create obb3 processor
+            processors["obb3_gt"] = Obb3GtProcessor(
+                obb3_file_path=gt_files["obb3_file"],
+                obb3_traj_file_path=gt_files["obb3_traj_file"],
+                instance_json_file_path=gt_files["instance_json_file"],
+                category_mapping_file_path=gt_files.get(
+                    "category_mapping_file", None
+                ),  # this file is optional
+                conf=conf.cubercnn_gt,
+            )
+
+            # Create obb2 processor, which requires streamid, pixel transforms, and calib from camera processors.
+            processors["obb2_gt"] = Obb2GtProcessor(
+                obb2_file_path=gt_files["obb2_file"],
+                instance_json_file_path=gt_files["instance_json_file"],
+                category_mapping_file_path=gt_files.get(
+                    "category_mapping_file", None
+                ),  # this file is optional
+                camera_label_to_stream_ids=selected_camera_label_to_stream_ids,
+                camera_label_to_pixel_transforms=selected_camera_label_to_pixel_transforms,
+                camera_label_to_calib=selected_camera_label_to_calib,
+                conf=conf.cubercnn_gt,
+            )
+
+        return processors
+
+    def get_sample_by_timestamp_ns(self, timestamp_ns) -> Optional[AtekDataSample]:
+        sample = AtekDataSample()
+
+        for processor_label, processor in self.processors.items():
+            if isinstance(processor, AriaCameraProcessor):
+                # ========================================
+                # Aria camera sensor data
+                # ========================================
+                sample_camera_data = processor.get_image_data_by_timestamps_ns(
+                    timestamps_ns=[timestamp_ns]
+                )
+                # Skip if no image data is available
+                if sample_camera_data is None:
+                    logger.warning(
+                        f"Querying camera for {timestamp_ns} on processor {processor_label} has returned None, skipping this sample."
+                    )
+                    return None
+
+                # Fill calibration data
+                sample_camera_data.camera_label = processor_label
+                sample_camera_data.origin_camera_label = processor.get_origin_label()
+                final_camera_calib = processor.get_final_camera_calib()
+                sample_camera_data.T_Device_Camera = torch.from_numpy(
+                    final_camera_calib.get_transform_device_camera().to_matrix3x4()
+                )
+                sample_camera_data.camera_model_name = str(
+                    final_camera_calib.model_name()
+                )
+                sample_camera_data.projection_params = torch.from_numpy(
+                    final_camera_calib.projection_params()
+                )
+
+                setattr(
+                    sample,
+                    processor_label.replace(
+                        "-", "_"
+                    ),  # field name in AtekDataSample is same as sensor label apart from a `-`->`_` conversion
+                    sample_camera_data,
+                )
+            # ========================================
+            # MPS traj data
+            # ========================================
+            elif isinstance(processor, MpsTrajProcessor):
+                maybe_mps_traj_data = processor.get_closed_loop_pose_by_timestamps_ns(
+                    [timestamp_ns]
+                )
+                if maybe_mps_traj_data is None:
+                    logger.warning(
+                        f"Querying MPS traj for {timestamp_ns} has returned None, skipping this sample."
+                    )
+                    return None
+
+                # Fill MPS traj data into sample
+                sample.mps_traj_data = MpsTrajData(
+                    Ts_World_Device=maybe_mps_traj_data[0],
+                    capture_timestamps_ns=maybe_mps_traj_data[1],
+                    gravity_in_world=maybe_mps_traj_data[2],
+                )
+
+            # ========================================
+            # GT data
+            # ========================================
+            elif isinstance(processor, Obb3GtProcessor):
+                maybe_gt_data = processor.get_gt_by_timestamp_ns(timestamp_ns)
+                if maybe_gt_data is None:
+                    logger.warning(
+                        f"Querying 3D Bbox GT data for {timestamp_ns} has returned None, skipping this sample."
+                    )
+                    return None
+                sample.gt_data["obb3_gt"] = maybe_gt_data
+
+            elif isinstance(processor, Obb2GtProcessor):
+                maybe_gt_data = processor.get_gt_by_timestamp_ns(timestamp_ns)
+                if maybe_gt_data is None:
+                    logger.warning(
+                        f"Querying 2D bbox GT data for {timestamp_ns} has returned None, skipping this sample."
+                    )
+                    return None
+                sample.gt_data["obb2_gt"] = maybe_gt_data
+
+            else:
+                raise ValueError(
+                    f"Unimplemented processor class {processor.__name__} in SampleBuilder! "
+                )
+
+        return sample
--- atek/data_preprocess/test/obb3_sample_builder_test.py
+++ atek/data_preprocess/test/obb3_sample_builder_test.py
@@ -8,15 +8,22 @@ import numpy as np
 
 import torch
 
+from atek.data_preprocess.processors.obb3_gt_processor import Obb3GtProcessor
+
+from atek.data_preprocess.sample_builders.cubercnn_sample_builder import (
+    CubeRCNNSampleBuilder,
+)
+
 from atek.data_preprocess.sample_builders.obb3_sample_builder import (
     ObjectDetection3dSampleBuilder,
 )
+
 from omegaconf import OmegaConf
 
 
 # test data paths
 TEST_DIR_PATH = os.path.join(os.getenv("TEST_FOLDER"))
-CONFIG_PATH = os.getenv("CONFIG_PATH")
+CONFIG_DIR = os.getenv("CONFIG_FOLDER")
 
 
 class Obb3SampleBuilderTest(unittest.TestCase):
@@ -24,7 +31,7 @@ class Obb3SampleBuilderTest(unittest.TestCase):
         super().setUp()
 
     def test_get_obb3_sample(self) -> None:
-        conf = OmegaConf.load(CONFIG_PATH)
+        conf = OmegaConf.load(os.path.join(CONFIG_DIR, "obb3_preprocess_base.yaml"))
 
         sample_builder = ObjectDetection3dSampleBuilder(
             conf=conf.processors,
@@ -54,3 +61,43 @@ class Obb3SampleBuilderTest(unittest.TestCase):
         self.assertFalse(hasattr(queried_sample, "camera_et_left"))  # No ET data
         self.assertTrue(queried_sample.mps_traj_data is not None)
         self.assertTrue(queried_sample.gt_data is not None)
+
+
+class CubeRCNNSampleBuilderTest(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+    def test_get_obb3_sample(self) -> None:
+        conf = OmegaConf.load(os.path.join(CONFIG_DIR, "cubercnn_preprocess_base.yaml"))
+
+        sample_builder = CubeRCNNSampleBuilder(
+            conf=conf.processors,
+            vrs_file=os.path.join(TEST_DIR_PATH, "test_ADT_unit_test_sequence.vrs"),
+            mps_files={
+                "mps_closedloop_traj_file": os.path.join(
+                    TEST_DIR_PATH, "test_ADT_trajectory.csv"
+                ),
+            },
+            gt_files={
+                "obb3_file": os.path.join(TEST_DIR_PATH, "test_3d_bounding_box.csv"),
+                "obb3_traj_file": os.path.join(
+                    TEST_DIR_PATH, "test_3d_bounding_box_traj.csv"
+                ),
+                "obb2_file": os.path.join(TEST_DIR_PATH, "test_2d_bounding_box.csv"),
+                "instance_json_file": os.path.join(
+                    TEST_DIR_PATH, "test_instances.json"
+                ),
+            },
+        )
+
+        queried_sample = sample_builder.get_sample_by_timestamp_ns(
+            timestamp_ns=87551170910000
+        )
+
+        self.assertTrue(queried_sample is not None)
+        self.assertTrue(queried_sample.camera_rgb is not None)
+        self.assertFalse(hasattr(queried_sample, "camera_et_left"))  # No ET data
+        self.assertTrue(queried_sample.mps_traj_data is not None)
+        self.assertCountEqual(queried_sample.gt_data.keys(), ["obb2_gt", "obb3_gt"])
+        for instance_id, instance_dict in queried_sample.gt_data["obb3_gt"].items():
+            self.assertCountEqual(instance_dict.keys(), Obb3GtProcessor.GT_DICT_KEYS)
