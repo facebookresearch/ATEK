fa0b29756b040615024c42ca21d8612772138398
Yang Lou <louy@meta.com>
2024-07-09 16:27:24 -0700

    Adding a new WDS loading unit test.
    Summary: This diff adds a unit test for WDS data loading, using temporary directory. It performs a round trip check, where data samples are written to WDS and loaded later, and their content before and after loading are compared directly.
    
    Reviewed By: echo-xiao9
    
    Differential Revision: D59313725
    
    fbshipit-source-id: a9c5051cce690cba2f7124b5bfc02c1329a8e841

---
+++ atek/data_loaders/test/atek_wds_dataloader_test.py
@@ -0,0 +1,133 @@
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+
+import os
+import tempfile
+import unittest
+
+import torch
+
+from atek.data_loaders.atek_wds_dataloader import load_atek_wds_dataset
+from atek.data_preprocess.atek_wds_writer import AtekWdsWriter
+from atek.data_preprocess.sample_builders.obb_sample_builder import ObbSampleBuilder
+from atek.data_preprocess.subsampling_lib.temporal_subsampler import (
+    CameraTemporalSubsampler,
+)
+from atek.data_preprocess.util.tensor_utils import check_dicts_same_w_tensors
+
+from omegaconf import OmegaConf
+
+# test data paths
+TEST_DIR_PATH = os.path.join(os.getenv("TEST_FOLDER"))
+CONFIG_PATH = os.getenv("CONFIG_PATH")
+CATEGORY_MAPPING_PATH = os.getenv("CATEGORY_MAPPING_PATH")
+
+
+class LoadAtekWdsDatasetTest(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.temp_dir_object = tempfile.TemporaryDirectory()
+        self.output_wds_path = self.temp_dir_object.name
+
+    def _preprocess_data(self) -> None:
+        """
+        A helper function to preprocess some ATEK data and write to WDS.
+        """
+        # From raw Aria dataset to WDS
+        conf = OmegaConf.load(CONFIG_PATH)
+
+        sample_builder = ObbSampleBuilder(
+            conf=conf.processors,
+            vrs_file=os.path.join(TEST_DIR_PATH, "test_ADT_unit_test_sequence.vrs"),
+            mps_files={
+                "mps_closedloop_traj_file": os.path.join(
+                    TEST_DIR_PATH, "test_ADT_trajectory.csv"
+                ),
+            },
+            gt_files={
+                "obb3_file": os.path.join(TEST_DIR_PATH, "test_3d_bounding_box.csv"),
+                "obb3_traj_file": os.path.join(
+                    TEST_DIR_PATH, "test_3d_bounding_box_traj.csv"
+                ),
+                "obb2_file": os.path.join(TEST_DIR_PATH, "test_2d_bounding_box.csv"),
+                "instance_json_file": os.path.join(
+                    TEST_DIR_PATH, "test_instances.json"
+                ),
+                "category_mapping_file": CATEGORY_MAPPING_PATH,
+            },
+        )
+
+        subsampler = CameraTemporalSubsampler(
+            vrs_file=os.path.join(TEST_DIR_PATH, "test_ADT_unit_test_sequence.vrs"),
+            conf=conf.camera_temporal_subsampler,
+        )
+
+        atek_wds_writer = AtekWdsWriter(
+            output_path=self.output_wds_path,
+            conf=conf.wds_writer,
+        )
+
+        # will be used for comparison after loading data
+        self.expected_flattened_samples = []
+
+        for i in range(subsampler.get_total_num_samples()):
+            timestamps_ns = subsampler.get_timestamps_by_sample_index(i)
+
+            for t in timestamps_ns:
+                sample = sample_builder.get_sample_by_timestamp_ns(t)
+                if sample is not None:
+                    atek_wds_writer.add_sample(data_sample=sample)
+                    self.expected_flattened_samples.append(sample.to_flatten_dict())
+        atek_wds_writer.close()
+
+    def test_atek_native_round_trip(self) -> None:
+        # Preprocess data and create WDS files
+        self._preprocess_data()
+
+        # check number of tars created
+        output_wds_names = os.listdir(self.output_wds_path)
+        self.assertEqual(len(output_wds_names), 1)  # only 1 tar file created
+
+        # Load tars back into ATEK
+        tar_list = [os.path.join(self.output_wds_path, f) for f in output_wds_names]
+        unbatched_dataset = load_atek_wds_dataset(
+            tar_list, batch_size=None, repeat_flag=False
+        )
+
+        for expected_sample, sample in zip(
+            self.expected_flattened_samples, unbatched_dataset
+        ):
+            # Check GT content
+            self.assertTrue(
+                check_dicts_same_w_tensors(
+                    expected_sample["gtdata.json"], sample["gtdata"], atol=1e-3
+                )
+            )
+            # Check other data content, note that expected_sample's keys have file extensions, like "gtdata.json",
+            # while ATEK loaded samples won't have extensions.
+            for key_w_extensions, expected_val in expected_sample.items():
+                # skip certain fields
+                if "image" in key_w_extensions or "gtdata" in key_w_extensions:
+                    continue
+
+                # remove file extension
+                key_no_extension = key_w_extensions.split(".")[0]
+                self.assertTrue(key_no_extension in sample)
+                sample_val = sample[key_no_extension]
+
+                if isinstance(expected_val, torch.Tensor):
+                    self.assertTrue(
+                        torch.allclose(
+                            expected_val.to(torch.float32),
+                            sample_val.to(torch.float32),
+                            atol=1e-3,
+                        )
+                    )
+                else:
+                    self.assertEqual(expected_val, sample_val)
+
+    # This is like a DTOR
+    def tearDown(self):
+        # Explicitly cleanup the temporary directory
+        self.temp_dir_object.cleanup()
+        print("Temporary directory deleted")
