af0528531f87aac45377532f98fab5405adbad85
Aria Kang <ariak@meta.com>
2024-07-24 10:04:26 -0700

    Create MPS online calib processor
    Summary:
    ## Context
    
    The stack of diffs is focused on adding new features and functionality to the MpsDataProvider library, specifically related to online calibration data. The overall purpose of the stack is to enhance the capabilities of the MpsDataProvider library for use in various applications.
    
    ## This Diff
    
    This diff creates a new processor for handling MPS online calibration data. The code changes include adding a new file to the `data_preprocess/processors/` directory and modifying the `TARGETS` file to include the new file. The new file contains a class called `MpsOnlineCalibProcessor` that will handle the online calibration data. The code changes are necessary to create a new processor that can handle the MPS online calibration data.
    
    Reviewed By: YLouWashU
    
    Differential Revision:
    D59832248
    
    Privacy Context Container: L1257120
    
    fbshipit-source-id: a90fa9cc99da05cc0c76f197cfab8bebce2cd713

---
+++ atek/data_preprocess/processors/mps_online_calib_processor.py
@@ -0,0 +1,93 @@
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+from typing import List, Optional
+
+import torch
+from atek.data_preprocess.atek_data_sample import MpsOnlineCalibData
+from omegaconf.omegaconf import DictConfig
+from projectaria_tools.core import mps
+from projectaria_tools.core.sensor_data import TimeQueryOptions  # @manual
+
+
+class MpsOnlineCalibProcessor:
+    """
+    processor to handle the MPS online calibration data
+    """
+
+    def __init__(
+        self,
+        mps_online_calib_data_file_path: str,
+        conf: DictConfig,
+    ):
+        self.conf = conf
+
+        mps_data_paths = mps.MpsDataPaths()
+        mps_data_paths.slam.online_calibrations = mps_online_calib_data_file_path
+        self.mps_data_provider = mps.MpsDataProvider(mps_data_paths)
+
+    def get_online_calibration_by_timestamps_ns(
+        self, timestamps_ns: List[int]
+    ) -> Optional[MpsOnlineCalibData]:
+        """
+        This function is to get online calibration data by a list of timestamps_ns
+        """
+
+        projection_params_list = []  # This will be a list of tensors
+        t_device_camera_list = []  # This will be a list of tensors
+        capture_timestamp_list = []  # This will store capture timestamps
+        utc_timestamp_list = []  # This will store UTC timestamps
+
+        for single_timestamp in timestamps_ns:
+            online_calib = self.mps_data_provider.get_online_calibration(
+                device_timestamp_ns=single_timestamp,
+                time_query_options=TimeQueryOptions.CLOSEST,
+            )
+
+            if online_calib is None:
+                continue
+
+            capture_timestamps_ns = int(
+                online_calib.tracking_timestamp.total_seconds() * 1_000_000_000
+            )
+            if abs(capture_timestamps_ns - single_timestamp) > self.conf.tolerance_ns:
+                continue
+            capture_timestamp_list.append(capture_timestamps_ns)
+            utc_timestamp_list.append(
+                int(online_calib.utc_timestamp.total_seconds() * 1_000_000_000)
+            )
+            projection_params_per_timestamp_list = []
+            t_device_camera_per_timestamp_list = []
+            camera_calibs = online_calib.camera_calibs
+
+            for camera_calib in camera_calibs:
+
+                projection_params = camera_calib.projection_params()
+                projection_params_per_timestamp_list.append(projection_params)
+
+                t_device_camera_per_timestamp_list.append(
+                    camera_calib.get_transform_device_camera().to_matrix3x4().tolist()
+                )
+
+            projection_params_list.append(
+                torch.tensor(projection_params_per_timestamp_list, dtype=torch.float32)
+            )
+            t_device_camera_list.append(
+                torch.tensor(t_device_camera_per_timestamp_list, dtype=torch.float32)
+            )
+
+        if len(projection_params_list) == 0:
+            return None
+
+        projection_params_tensor = torch.stack(projection_params_list)
+        t_device_camera_tensor = torch.stack(t_device_camera_list)
+        capture_timestamp_tensor = torch.tensor(
+            capture_timestamp_list, dtype=torch.int64
+        )
+        utc_timestamp_tensor = torch.tensor(utc_timestamp_list, dtype=torch.int64)
+
+        return MpsOnlineCalibData(
+            capture_timestamps_ns=capture_timestamp_tensor,
+            utc_timestamps_ns=utc_timestamp_tensor,
+            projection_params=projection_params_tensor,
+            t_device_camera=t_device_camera_tensor,
+        )
