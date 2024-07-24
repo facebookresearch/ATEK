91b90195dde68832ed1bebb5d35183aae56e25d0
Aria Kang <ariak@meta.com>
2024-07-23 17:50:26 -0700

    test WDS writing for MpsOnlineCalibData
    Summary:
    ## Context
    
    The stack of diffs is focused on adding new features and functionality to the MpsDataProvider library, specifically related to online calibration data. The overall purpose of the stack is to enhance the capabilities of the MpsDataProvider library for use in various applications.
    
    ## This Diff
    
    This diff is titled "[ATEK] test WDS writing for MpsOnlineCalibData" and contains code changes in the file "atek_data_sample_test.py". The changes include adding a new class "MpsOnlineCalibData" to the file and testing the writing of this class to a Web Data Service (WDS) using the "write_to_wds" function. The changes are intended to improve the functionality of the ATEK data preprocessing pipeline.
    
    Reviewed By: YLouWashU
    
    Differential Revision:
    D59832249
    
    Privacy Context Container: L1257120
    
    fbshipit-source-id: 498781d786b9f663ba608c4f4b8d9cde816f6e02

--- atek/data_preprocess/test/atek_data_sample_test.py
+++ atek/data_preprocess/test/atek_data_sample_test.py
@@ -7,6 +7,7 @@ import torch
 
 from atek.data_preprocess.atek_data_sample import (
     AtekDataSample,
+    MpsOnlineCalibData,
     MpsSemiDensePointData,
     MpsTrajData,
     MultiFrameCameraData,
@@ -104,6 +105,93 @@ class AtekDataSampleTest(unittest.TestCase):
             gravity_in_world=torch.tensor([0, 0, -9.81]),
         )
 
+        mps_online_calib_data = MpsOnlineCalibData(
+            capture_timestamps_ns=torch.tensor([14580434854000]),
+            utc_timestamps_ns=torch.tensor([0], dtype=torch.int64),
+            projection_params=torch.tensor(
+                [
+                    [
+                        [
+                            2.4138e02,
+                            3.1623e02,
+                            2.3668e02,
+                            -2.7658e-02,
+                            1.0371e-01,
+                            -7.3428e-02,
+                            1.2858e-02,
+                            1.3407e-03,
+                            -4.5717e-04,
+                            4.4453e-04,
+                            -1.9325e-03,
+                            1.5349e-04,
+                            2.6054e-04,
+                            3.3760e-03,
+                            1.3415e-04,
+                        ],
+                        [
+                            2.4104e02,
+                            3.1769e02,
+                            2.3818e02,
+                            -2.4638e-02,
+                            9.5806e-02,
+                            -6.5053e-02,
+                            9.1013e-03,
+                            1.9200e-03,
+                            -4.4897e-04,
+                            1.5518e-03,
+                            -5.5009e-04,
+                            -8.7365e-04,
+                            2.1657e-04,
+                            1.1816e-03,
+                            1.9690e-04,
+                        ],
+                        [
+                            1.2219e03,
+                            1.4627e03,
+                            1.4659e03,
+                            4.0604e-01,
+                            -4.8995e-01,
+                            1.7457e-01,
+                            1.1330e00,
+                            -1.7016e00,
+                            6.5116e-01,
+                            6.2115e-04,
+                            1.9322e-05,
+                            -1.4855e-05,
+                            2.6012e-04,
+                            -6.5821e-04,
+                            3.7614e-05,
+                        ],
+                    ]
+                ],
+                dtype=torch.float64,
+            ),
+            t_device_camera=torch.tensor(
+                [
+                    [
+                        [
+                            [9.9998e-01, 5.5086e-04, -5.9830e-03, 4.3290e-04],
+                            [-6.2809e-04, 9.9992e-01, -1.2914e-02, -9.0162e-05],
+                            [5.9753e-03, 1.2917e-02, 9.9990e-01, -4.8937e-05],
+                            [0.0000e00, 0.0000e00, 0.0000e00, 1.0000e00],
+                        ],
+                        [
+                            [9.9845e-01, -3.4170e-02, 4.3828e-02, 6.0829e-03],
+                            [5.0779e-02, 2.4046e-01, -9.6933e-01, -1.1159e-01],
+                            [2.2584e-02, 9.7006e-01, 2.4182e-01, -8.7817e-02],
+                            [0.0000e00, 0.0000e00, 0.0000e00, 1.0000e00],
+                        ],
+                        [
+                            [9.9339e-01, -5.1506e-02, 1.0257e-01, -4.2807e-03],
+                            [1.0364e-01, 7.8649e-01, -6.0884e-01, -1.1842e-02],
+                            [-4.9313e-02, 6.1545e-01, 7.8663e-01, -5.1140e-03],
+                            [0.0000e00, 0.0000e00, 0.0000e00, 1.0000e00],
+                        ],
+                    ]
+                ]
+            ),
+        )
+
         gt_data = {"key_1": "value_1", "key_2": "value_2"}
 
         data = AtekDataSample(
@@ -111,6 +199,7 @@ class AtekDataSampleTest(unittest.TestCase):
             camera_slam_left=slam_left_data,
             mps_traj_data=mps_traj_data,
             gt_data=gt_data,
+            mps_online_calib_data=mps_online_calib_data,
         )
 
         data_dict = data.to_flatten_dict()
@@ -130,5 +219,9 @@ class AtekDataSampleTest(unittest.TestCase):
             "mtd#ts_world_device.pth",
             "mtd#gravity_in_world.pth",
             "gtdata.json",
+            "moc#capture_timestamps_ns.pth",
+            "moc#utc_timestamps_ns.pth",
+            "moc#projection_params.pth",
+            "moc#t_device_camera.pth",
         ]
         self.assertCountEqual(data_dict.keys(), expected_keys)
