88a18917a44ada42c90a5880d10d57915c777268
Yang Lou <louy@meta.com>
2024-07-30 18:39:03 -0700

    Refactor inference script to CubeRCNN example.
    Summary: This diff refactors the inference script for CubeRCNN example. The new inference script simply loops over the data samples, run model prediction, and plot the results.
    
    Reviewed By: echo-xiao9
    
    Differential Revision: D59769530
    
    fbshipit-source-id: d12fafd231280521bc952d894e7530c32a266ab3

--- tools/infer_cubercnn.py
+++ tools/infer_cubercnn.py
@@ -3,85 +3,169 @@ import json
 import os
 import sys
 import time
+from copy import deepcopy
+from dataclasses import fields
+
+import torch
 
 import tqdm
+import yaml
 
-from atek.dataset.dataset_factory import create_inference_dataset
-from atek.inference.callback_factory import create_inference_callback
-from atek.model.model_factory import create_inference_model
+from atek.data_loaders.atek_wds_dataloader import load_atek_wds_dataset
 
+from atek.data_loaders.cubercnn_model_adaptor import (
+    cubercnn_collation_fn,
+    CubeRCNNModelAdaptor,
+    load_atek_wds_dataset_as_cubercnn,
+)
+from atek.data_preprocess.atek_data_sample import (
+    create_atek_data_sample_from_flatten_dict,
+)
+
+from atek.viz.atek_visualizer import NativeAtekSampleVisualizer
+
+from cubercnn.config import get_cfg_defaults
+from cubercnn.modeling.backbone import build_dla_from_vision_fpn_backbone  # noqa
+from cubercnn.modeling.meta_arch import build_model  # noqa
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.config import get_cfg
 from detectron2.engine import launch
-from detectron2.utils import comm
-from torch.nn.parallel import DistributedDataParallel
+
+from omegaconf import DictConfig, OmegaConf
 
 
-def main(args):
+def create_inference_model(config_file, ckpt_dir):
+    """
+    Create the model for inference pipeline, with the model config.
+    """
+    # Create default model configuration
+    model_config = get_cfg()
+    get_cfg_defaults(model_config)
+
+    # add extra configs for data
+    model_config.MAX_TRAINING_ATTEMPTS = 3
+    model_config.TRAIN_LIST = ""
+    model_config.TEST_LIST = ""
+    model_config.ID_MAP_JSON = ""
+    model_config.OBJ_PROP_JSON = ""
+    model_config.CATEGORY_JSON = ""
+    model_config.DATASETS.OBJECT_DETECTION_MODE = ""
+    model_config.SOLVER.VAL_MAX_ITER = 0
+
+    model_config.merge_from_file(config_file)
+    model_config.freeze()
+
+    model = build_model(model_config, priors=None)
+
+    _ = DetectionCheckpointer(model, save_dir=ckpt_dir).resume_or_load(
+        model_config.MODEL.WEIGHTS, resume=True
+    )
+    model.eval()
+
+    return model_config, model
+
+
+def get_tars(tar_yaml, use_relative_path=False):
+    with open(tar_yaml, "r") as f:
+        tar_files = yaml.safe_load(f)["tars"]
+    if use_relative_path:
+        data_dir = os.path.dirname(tar_yaml)
+        tar_files = [os.path.join(data_dir, x) for x in tar_files]
+    return tar_files
+
+
+def run_inference(args):
+    # parse in config file
+    conf = OmegaConf.load(args.config_file)
+
     # setup config and model
-    model_config, model = create_inference_model(args)
+    model_config, model = create_inference_model(args.config_file, args.ckpt_dir)
 
-    # setup distributed inference
-    world_size = comm.get_world_size()
-    rank = comm.get_rank()
-    if world_size > 1:
-        model = DistributedDataParallel(
-            model,
-            device_ids=[comm.get_local_rank()],
-            broadcast_buffers=False,
-            find_unused_parameters=True,
-        )
+    # set up data loader from eval dataset
+    infer_tars = get_tars(args.input_wds_tar, use_relative_path=True)
+    # For testing only
+    infer_tars = infer_tars[:10]
 
-    # setup callbacks
-    callbacks = create_inference_callback(args, model_config)
+    infer_wds_atek_format = load_atek_wds_dataset(
+        urls=infer_tars,
+        batch_size=1,
+        repeat_flag=False,
+        collation_fn=cubercnn_collation_fn,
+    )
+    infer_wds_cubercnn_format = load_atek_wds_dataset_as_cubercnn(
+        urls=infer_tars, batch_size=1, repeat_flag=False
+    )
 
-    # load sequence paths for inference
-    with open(args.input_file, "r") as f:
-        seq_paths_all = f.read().splitlines()
+    atek_format_dataloader = torch.utils.data.DataLoader(
+        infer_wds_atek_format,
+        batch_size=None,
+        num_workers=conf.DATALOADER.NUM_WORKERS,
+        pin_memory=True,
+    )
+    cubercnn_format_dataloader = torch.utils.data.DataLoader(
+        infer_wds_cubercnn_format,
+        batch_size=None,
+        num_workers=conf.DATALOADER.NUM_WORKERS,
+        pin_memory=True,
+    )
 
-    # setup profiling
+    # Loop over all batched samples in the dataset
+    atek_viz = NativeAtekSampleVisualizer()
+    # Setup profiling
     num_iters = 0
     num_samples = 0
     start_time = time.time()
 
-    # run inference
-    seq_paths_local = seq_paths_all[rank::world_size]
-    for seq_path in tqdm.tqdm(seq_paths_local):
-        # setup dataset
-        dataset = create_inference_dataset(seq_path, args, model_config)
+    for atek_native_data, data in tqdm.tqdm(
+        zip(atek_format_dataloader, cubercnn_format_dataloader),
+        desc="Inference progress: ",
+    ):
+        num_iters += 1
+        model_output = model(data)
 
-        prediction_list = []
-        for data in dataset:
-            model_output = model(data)
+        # Batched samples are formulated as lists in CubeRCNN, hence fill predictions over the lists
+        for single_atek_input, single_cubercnn_input, single_cubercnn_output in zip(
+            atek_native_data, data, model_output
+        ):
+            num_samples += 1
+            gt_sample = create_atek_data_sample_from_flatten_dict(single_atek_input)
+            prediction_in_atek_format = CubeRCNNModelAdaptor.cubercnn_gt_to_atek_gt(
+                cubercnn_pred_dict=single_cubercnn_output,
+                T_world_camera_np=single_cubercnn_input["T_world_camera"],
+                camera_label="camera-rgb",
+            )
 
-            # post-process model output
-            prediction = callbacks["post_processor"](data, model_output)
+            # Filter out prediction with low confidence scores
+            # TODO: refactor this. Very ugly
+            valid_viz_filter = prediction_in_atek_format["scores"] > 0.8
+            for key, val in prediction_in_atek_format["obb2_gt"]["camera-rgb"].items():
+                if isinstance(val, torch.Tensor):
+                    prediction_in_atek_format["obb2_gt"]["camera-rgb"][key] = val[
+                        valid_viz_filter
+                    ]
 
-            # run callbacks for current iteration
-            for callback in callbacks["per_batch_callbacks"]:
-                callback(data, prediction)
+            for key, val in prediction_in_atek_format["obb3_gt"]["camera-rgb"].items():
+                if isinstance(val, torch.Tensor):
+                    prediction_in_atek_format["obb3_gt"]["camera-rgb"][key] = val[
+                        valid_viz_filter
+                    ]
 
-            for eval_callback in callbacks["eval_callbacks"]:
-                eval_callback.update(data, prediction)
+            # Visualize the GT results
+            atek_viz.plot_atek_sample(
+                atek_data_sample=gt_sample,
+                plot_line_color=NativeAtekSampleVisualizer.COLOR_GREEN,
+                suffix="_gt",
+            )
 
-            prediction_list.append(prediction)
+            # Visualize the prediction results, only visualize the GT part
+            atek_viz.plot_gtdata(
+                atek_gt_dict=prediction_in_atek_format,
+                timestamp_ns=gt_sample.camera_rgb.capture_timestamps_ns.item(),
+                plot_line_color=NativeAtekSampleVisualizer.COLOR_RED,
+                suffix="_infer",
+            )
 
-            # increment iteration and sample counter
-            num_iters += 1
-            num_samples += len(data)
-
-        # run callbacks for whole sequence
-        for callback in callbacks["per_sequence_callbacks"]:
-            callback(prediction_list)
-
-    eval_results = []
-    for eval_callback in callbacks["eval_callbacks"]:
-        eval_results.append(eval_callback.evaluate())
-
-    print(f"Evaluation results:\n{eval_results}")
-    if args.eval_save_path:
-        assert args.eval_save_path.endswith(".json")
-        with open(args.eval_save_path, "w") as f:
-            json.dump(eval_results, f, indent=2)
-
+    # print time information
     elapsed_time = time.time() - start_time
     profile_message = (
         f"Inference time: {elapsed_time:.3f} secs for {num_iters} iters, "
@@ -92,115 +176,28 @@ def main(args):
     print(profile_message)
 
 
-# TODO: make this more generic and rename the file to infer.py
 def get_args():
+    """
+    Parse input arguments from CLI.
+    """
     parser = argparse.ArgumentParser(
         epilog=None,
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument(
-        "--input-file", default=None, help="Path to file with test sequences"
+        "--input-wds-tar", default=None, help="Input WDS tar files to run inference on."
     )
     parser.add_argument(
         "--output-dir", default=None, help="Directory to save model predictions"
     )
     parser.add_argument(
-        "--data-type",
-        default="raw",
-        help="Input data type. wds: webdataset tars, raw: raw ADT data",
-    )
-    parser.add_argument(
-        "--rotate_image_cw90deg",
-        type=bool,
-        default=True,
-        help="Rotate images by 90 degrees clockwise",
-    )
-    parser.add_argument(
-        "--height",
-        type=int,
-        default=512,
-        help="Target image height to process the raw data",
-    )
-    parser.add_argument(
-        "--width",
-        type=int,
-        default=512,
-        help="Target image width to process the raw data",
-    )
-    parser.add_argument(
-        "--model-name",
-        default=None,
-        help="Model architecture name, e.g., cubercnn",
+        "--ckpt-dir", default=None, help="Directory for model checkpoint"
     )
     parser.add_argument(
         "--config-file",
         default=None,
         metavar="FILE",
-        help="Path to config file of a trained model",
-    )
-    parser.add_argument(
-        "--metadata-file",
-        default=None,
-        help="File with metadata for all instances",
-    )
-    parser.add_argument(
-        "--threshold",
-        type=float,
-        default=0.25,
-        help="threshold on score for visualizing",
-    )
-    parser.add_argument(
-        "--save-prediction",
-        default=False,
-        action="store_true",
-        help="Whether to save prediction",
-    )
-    parser.add_argument(
-        "--visualize",
-        default=False,
-        action="store_true",
-        help="whether to visualize inference results",
-    )
-    parser.add_argument(
-        "--web-port",
-        default=8888,
-        help="The port to serve the web viewer on (defaults to 8888).",
-    )
-    parser.add_argument(
-        "--ws-port",
-        default=8877,
-        help="The port to serve the WebSocket server on (defaults to 8877)",
-    )
-    parser.add_argument(
-        "--evaluate",
-        default=False,
-        action="store_true",
-        help="whether to evaluate the model predictions",
-    )
-    parser.add_argument(
-        "--metric-name",
-        type=str,
-        default="iou",
-        choices=[
-            "iou",
-            "giou",
-            "box_corner_chamfer_distance",
-            "box_corner_hungarian_distance",
-            "center_euclidean_distance",
-        ],
-        help="Name of the metric to use for 3D bounding box evaluation",
-    )
-    parser.add_argument(
-        "--metric-threshold",
-        type=float,
-        default=0.25,
-        help="Threshold of metric for matching predicted and GT bounding boxes",
-    )
-    parser.add_argument(
-        "--eval-save-path",
-        type=str,
-        default=None,
-        help="Path to save evaluation results",
+        help="Path to Omega config file",
     )
     parser.add_argument(
         "--num-gpus", type=int, default=1, help="number of gpus *per machine*"
@@ -214,13 +211,6 @@ def get_args():
         default=0,
         help="the rank of this machine (unique per machine)",
     )
-    port = 2**15 + 2**14 + hash(os.getuid() if sys.platform != "win32" else 1) % 2**14
-    parser.add_argument(
-        "--dist-url",
-        default="tcp://127.0.0.1:{}".format(port),
-        help="initialization URL for pytorch distributed backend. See "
-        "https://pytorch.org/docs/stable/distributed.html for details.",
-    )
 
     return parser.parse_args()
 
@@ -229,11 +219,11 @@ if __name__ == "__main__":
     args = get_args()
 
     print("Command Line Args:", args)
+
     launch(
-        main,
+        run_inference,
         args.num_gpus,
         num_machines=args.num_machines,
         machine_rank=args.machine_rank,
-        dist_url=args.dist_url,
         args=(args,),
     )
