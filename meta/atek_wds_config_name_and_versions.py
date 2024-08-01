9af7cbb79928fb5e99df3ba8e7e3b15f167441e0
Aria Kang <ariak@meta.com>
2024-08-01 08:56:32 -0700

    Add ATEK_Metadata.json generation in Preprocessing workflow
    Summary: I added an atek_metadata.json to describe the wds file generated from  atek data sample. raw_dataset_name, raw_dataset_release, atek_preprocessing_config_name, atek_wds_version, atek_preprocessing_config_content, atek_wds_shapes and atek_wds_dtypes
    
    Reviewed By: YLouWashU
    
    Differential Revision:
    D60529959
    
    Privacy Context Container: L1257120
    
    fbshipit-source-id: 35ae66a8320be03ef958f88bb0656d0577b959c9

---
+++ meta/atek_wds_config_name_and_versions.py
@@ -0,0 +1,19 @@
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+# pyre-strict
+
+ATEK_CONFIGS_AND_LASTEST_VERSIONS = {
+    "cubercnn": "v0.1.0",
+    "efm": "v0.1.0",
+}
+
+"""
+class AtekWdsConfigNameAndVersions:
+    
+    A class to record configuration for ATEK WDS dataset.
+    
+        self.config_name_and_lastest_version = {
+            "cubercnn": "v0.1.0",
+            "efm": "v0.1.0",
+        }
+"""
