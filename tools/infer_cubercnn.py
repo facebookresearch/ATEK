2bb997844403bb5980c958de72f07dc35efadb53
Yang Lou <louy@meta.com>
2024-08-09 18:15:21 -0700

    Improve infer.py by adding batching, pred+gt file writing, etc. (#13)
    Summary:
    Pull Request resolved: https://github.com/fairinternal/ATEK/pull/13
    
    As title.
    
    Reviewed By: echo-xiao9
    
    Differential Revision: D60206903

--- tools/infer_cubercnn.py
+++ tools/infer_cubercnn.py
@@ -1,13 +1,14 @@
 import argparse
 import json
+import logging
 import os
 import sys
 import time
 from copy import deepcopy
 from dataclasses import fields
+from typing import Dict
 
 import torch
-
 import tqdm
 import yaml
 
@@ -21,6 +22,9 @@ from atek.data_loaders.cubercnn_model_adaptor import (
 from atek.data_preprocess.atek_data_sample import (
     create_atek_data_sample_from_flatten_dict,
 )
+from atek.evaluation.static_object_detection.obb3_csv_io import GroupAtekObb3CsvWriter
+from atek.util.atek_constants import ATEK_CATEGORY_NAME_TO_ID
+from atek.util.file_io_utils import load_category_mapping_from_csv
 
 from atek.viz.atek_visualizer import NativeAtekSampleVisualizer
 
@@ -32,6 +36,17 @@ from detectron2.config import get_cfg
 from detectron2.engine import launch
 
 from omegaconf import DictConfig, OmegaConf
+
+
+logging.basicConfig(
+    level=logging.INFO,  # Set the minimum log level to DEBUG
+    format="%(asctime)s-%(levelname)s:%(message)s",  # Format of the log messages
+    handlers=[
+        logging.StreamHandler(),  # Output logs to console
+    ],
+)
+
+logger = logging.getLogger("infer_cubercnn")
 
 
 def create_inference_model(config_file, ckpt_dir, use_cpu_only=False):
@@ -76,6 +91,38 @@ def get_tars(tar_yaml, use_relative_path=False):
     return tar_files
 
 
+def parse_cubercnn_id_to_atek_id_mapping(
+    model_config_file: str, ase_to_atek_file: str
+) -> Dict[int, int]:
+    """
+    Helper function to handle the case where model output category mapping is different from ATEK's taxonomy.
+    Model output taxonomy is stored under the model config.yaml file
+    """
+    with open(model_config_file, "r") as f:
+        data = yaml.safe_load(f)
+    # Extract the category names
+    cat_mapping = data["DATASETS"]["CATEGORY_NAMES"]
+    # Create a dictionary with IDs starting from 0
+    cubercnn_id_to_name = {i: name for i, name in enumerate(cat_mapping)}
+
+    cubercnn_id_to_atek_id = {}
+    # Load in ase_to_atek name mapping
+    ase_to_atek_mapping = load_category_mapping_from_csv(
+        category_mapping_csv_file=ase_to_atek_file
+    )
+
+    for i, name in cubercnn_id_to_name.items():
+        atek_name, atek_id = ase_to_atek_mapping[name]
+        if atek_name not in ATEK_CATEGORY_NAME_TO_ID:
+            raise ValueError(
+                f"category name {name} mapped to {atek_name}, still not in ATEK mapping!"
+            )
+
+        cubercnn_id_to_atek_id[int(i)] = int(atek_id)
+
+    return cubercnn_id_to_atek_id
+
+
 def run_inference(args):
     # parse in config file
     conf = OmegaConf.load(args.config_file)
@@ -85,19 +132,25 @@ def run_inference(args):
         args.config_file, args.ckpt_dir, args.num_gpus == 0
     )
 
-    # set up data loader from eval dataset
-    infer_tars = get_tars(args.input_wds_tar, use_relative_path=True)
-    # For testing only
-    infer_tars = infer_tars[:10]
+    # (Optional) parse in category mapping from model output to ATEK
+    # TODO: this is hard-coded, remove this
+    """
+    cubercnn_id_to_atek_id = parse_cubercnn_id_to_atek_id_mapping(
+        args.config_file,
+        ase_to_atek_file="/home/louy/Calibration_data_link/Atek/2024_07_30_EvalTest/ase_to_atek.csv",
+    )
+    """
 
+    # set up data loaders from eval dataset
+    infer_tars = get_tars(args.input_wds_tar, use_relative_path=True)
     infer_wds_atek_format = load_atek_wds_dataset(
         urls=infer_tars,
-        batch_size=1,
+        batch_size=args.batch_size,
         repeat_flag=False,
         collation_fn=cubercnn_collation_fn,
     )
     infer_wds_cubercnn_format = load_atek_wds_dataset_as_cubercnn(
-        urls=infer_tars, batch_size=1, repeat_flag=False
+        urls=infer_tars, batch_size=args.batch_size, repeat_flag=False
     )
 
     atek_format_dataloader = torch.utils.data.DataLoader(
@@ -113,52 +166,115 @@ def run_inference(args):
         pin_memory=True,
     )
 
-    # Loop over all batched samples in the dataset
-    atek_viz = NativeAtekSampleVisualizer()
+    # set up visualization
+    if args.viz_flag:
+        atek_viz = NativeAtekSampleVisualizer()
+
+    # set up prediction writer
+    pred_writer = GroupAtekObb3CsvWriter(
+        output_folder=args.output_dir, output_filename="prediction_obbs.csv"
+    )
+    gt_writer = GroupAtekObb3CsvWriter(
+        output_folder=args.output_dir, output_filename="gt_obbs.csv"
+    )
+
     # Setup profiling
     num_iters = 0
     num_samples = 0
     start_time = time.time()
+    current_sequence_name = ""
 
+    # Loop over all batched samples in the dataset
     for atek_native_data, data in tqdm.tqdm(
         zip(atek_format_dataloader, cubercnn_format_dataloader),
         desc="Inference progress: ",
     ):
         num_iters += 1
-        model_output = model(data)
 
-        # Batched samples are formulated as lists in CubeRCNN, hence fill predictions over the lists
-        for single_atek_input, single_cubercnn_input, single_cubercnn_output in zip(
-            atek_native_data, data, model_output
-        ):
-            num_samples += 1
-            gt_sample = create_atek_data_sample_from_flatten_dict(single_atek_input)
-            prediction_in_atek_format = CubeRCNNModelAdaptor.cubercnn_gt_to_atek_gt(
-                cubercnn_pred_dict=single_cubercnn_output,
-                T_world_camera_np=single_cubercnn_input["T_world_camera"],
-                camera_label="camera-rgb",
-            )
+        # Log time
+        if num_iters % 100 == 0:
+            end_time = time.time()
+            logger.info(f"Time for iteration {num_iters} is {end_time - start_time}s")
+            end_time = start_time
 
-            # Filter out prediction with low confidence scores
-            # TODO: refactor this. Very ugly
-            valid_viz_filter = prediction_in_atek_format["scores"] > 0.8
-            for key, val in prediction_in_atek_format["obb2_gt"]["camera-rgb"].items():
-                if isinstance(val, torch.Tensor):
-                    prediction_in_atek_format["obb2_gt"]["camera-rgb"][key] = val[
-                        valid_viz_filter
-                    ]
+        try:
+            model_output = model(data)
 
-            for key, val in prediction_in_atek_format["obb3_gt"]["camera-rgb"].items():
-                if isinstance(val, torch.Tensor):
-                    prediction_in_atek_format["obb3_gt"]["camera-rgb"][key] = val[
-                        valid_viz_filter
-                    ]
+            # Batched samples are formulated as lists in CubeRCNN, hence fill predictions over the lists
+            for single_atek_input, single_cubercnn_input, single_cubercnn_output in zip(
+                atek_native_data, data, model_output
+            ):
+                num_samples += 1
+                gt_sample = create_atek_data_sample_from_flatten_dict(single_atek_input)
+                timestamp_ns = gt_sample.camera_rgb.capture_timestamps_ns.item()
+                prediction_in_atek_format = CubeRCNNModelAdaptor.cubercnn_gt_to_atek_gt(
+                    cubercnn_pred_dict=single_cubercnn_output,
+                    T_world_camera_np=single_cubercnn_input["T_world_camera"],
+                    camera_label="camera-rgb",
+                    # cubercnn_id_to_atek_id=cubercnn_id_to_atek_id,
+                )
 
-            # Visualize the GT results
-            atek_viz.plot_atek_sample(
-                atek_data_sample=gt_sample,
-                plot_line_color=NativeAtekSampleVisualizer.COLOR_GREEN,
-                suffix="_gt",
+                # Some printing info
+                if current_sequence_name != single_atek_input["sequence_name"]:
+                    current_sequence_name = single_atek_input["sequence_name"]
+                    logger.info(
+                        f"Starting to process sequence: {current_sequence_name}"
+                    )
+
+                # --------------------- Update CsvWriter ------------------------#
+                gt_writer.write_from_atek_dict(
+                    atek_dict=single_atek_input["gt_data"]["obb3_gt"]["camera-rgb"],
+                    timestamp_ns=timestamp_ns,
+                    sequence_name=single_atek_input["sequence_name"],
+                )
+
+                pred_writer.write_from_atek_dict(
+                    atek_dict=prediction_in_atek_format["obb3_gt"]["camera-rgb"],
+                    confidence_score=prediction_in_atek_format["scores"],
+                    timestamp_ns=timestamp_ns,
+                    sequence_name=single_atek_input["sequence_name"],
+                )
+
+                # --------------------- Update Viz ------------------------#
+                # Filter out prediction with low confidence scores
+                if args.viz_flag:
+                    # TODO: refactor this. Very ugly
+                    valid_viz_filter = prediction_in_atek_format["scores"] > 0.8
+                    for key, val in prediction_in_atek_format["obb2_gt"][
+                        "camera-rgb"
+                    ].items():
+                        if isinstance(val, torch.Tensor):
+                            prediction_in_atek_format["obb2_gt"]["camera-rgb"][key] = (
+                                val[valid_viz_filter]
+                            )
+
+                    for key, val in prediction_in_atek_format["obb3_gt"][
+                        "camera-rgb"
+                    ].items():
+                        if isinstance(val, torch.Tensor):
+                            prediction_in_atek_format["obb3_gt"]["camera-rgb"][key] = (
+                                val[valid_viz_filter]
+                            )
+
+                    # Visualize the GT results
+                    atek_viz.plot_atek_sample(
+                        atek_data_sample=gt_sample,
+                        plot_line_color=NativeAtekSampleVisualizer.COLOR_GREEN,
+                        suffix="_gt",
+                    )
+
+                    # Visualize the prediction results, only visualize the GT part
+                    atek_viz.plot_gtdata(
+                        atek_gt_dict=prediction_in_atek_format,
+                        timestamp_ns=gt_sample.camera_rgb.capture_timestamps_ns.item(),
+                        plot_line_color=NativeAtekSampleVisualizer.COLOR_RED,
+                        suffix="_infer",
+                    )
+
+        except Exception as e:
+            logger.error(
+                f"processing failed for iter {num_iters}, for either {current_sequence_name} or the sequence after this. Skipping"
+                f"error message: {e}"
             )
             # put pred_samples' category name and confidence together in obb2_gt and obb3_gt
             for i in range(
@@ -177,13 +293,8 @@ def run_inference(args):
                 prediction_in_atek_format["obb3_gt"]["camera-rgb"]["category_names"][
                     i
                 ] += f": {prediction_in_atek_format['scores'][i]:.2f}"
-            # Visualize the prediction results, only visualize the GT part
-            atek_viz.plot_gtdata(
-                atek_gt_dict=prediction_in_atek_format,
-                timestamp_ns=gt_sample.camera_rgb.capture_timestamps_ns.item(),
-                plot_line_color=NativeAtekSampleVisualizer.COLOR_RED,
-                suffix="_infer",
-            )
+    gt_writer.flush()
+    pred_writer.flush()
 
     # print time information
     elapsed_time = time.time() - start_time
@@ -218,6 +329,17 @@ def get_args():
         default=None,
         metavar="FILE",
         help="Path to Omega config file",
+    )
+    parser.add_argument(
+        "--viz-flag",
+        action="store_true",
+        help=" Flag to control if show visualization (default: False)",
+    )
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=1,
+        help=" Batch size to run inference in GPU",
     )
     parser.add_argument(
         "--num-gpus", type=int, default=1, help="number of gpus *per machine*"
