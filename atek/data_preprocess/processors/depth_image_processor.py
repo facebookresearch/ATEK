Fix overflow issue when reading depth values
Summary:
The call to calibration.distort_by_calibration inside of AriaCameraProcessor.DistortByCalibrationTVWrapper would convert int32 and uint32 to uint8, losing depth information by overflowing.

Ammends the transformation by upcasting to uint64 to make sure no information is lost.

Reviewed By: YLouWashU

Differential Revision: D60428658

fbshipit-source-id: 988900e015a1d40e520832aa408781e13b501369

atek/data_preprocess/processors/depth_image_processor.py
@@ -85,7 +85,7 @@ class DepthImageProcessor:
             # Handle uint16 not supported by torch
             np_image = image_data_and_record[0].to_numpy_array()
             if np_image.dtype == np.uint16:
-                np_image = np_image.astype(np.int32)
+                np_image = np_image.astype(np.uint32)
             image = torch.from_numpy(np_image)
             if len(image.shape) == 2:
                 # single channel image: [h,w] -> [c, h, w]