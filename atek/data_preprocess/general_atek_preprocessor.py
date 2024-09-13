87eba458430f16540e30c31352729fd98be6e19e
Yang Lou <louy@fb.com>
2024-09-13 00:24:55 -0700

    Fix bugs in viz lib import
    Differential Revision: D62626090

--- atek/data_preprocess/general_atek_preprocessor.py
+++ atek/data_preprocess/general_atek_preprocessor.py
@@ -1,4 +1,17 @@
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 # pyre-strict
 
@@ -7,7 +20,7 @@ from typing import Optional
 
 from atek.data_preprocess.atek_data_sample import AtekDataSample
 from atek.data_preprocess.atek_wds_writer import AtekWdsWriter
-from atek.viz.atek_visualizer_base import NativeAtekSampleVisualizer
+from atek.viz.atek_visualizer import NativeAtekSampleVisualizer
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
